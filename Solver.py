b05ee0b82b7aab2fd095a6c2fbe747c490f03e84
duckducksillygoose <Ashleigh.Gurr@student.curtin.edu.au>
2025-02-24 10:44:24 +0800

    Moved all of the required solving mechanisms to solver.py

--- Solver.py
+++ Solver.py
@@ -1,6 +1,20 @@
 import numpy as np
 import random
-from model import cube_import
-from model import TL, TR, BL, BR, LU, LD, RU, RD
+from model import *
 
 cube_import()
+
+reward = 0 
+
+s_moves=[]
+#solution
+for i  in range (10):
+    step = random.choice(moves)
+    s_moves.append(step)
+    if s_moves[i] != moves_made[i]:
+        reward = reward - 1
+
+    else:
+        reward = reward + 1
+
+print(reward)
--- model.py
+++ model.py
@@ -94,20 +94,3 @@ def cube_import():
     print(solve_state)
 
 
-#reversal manouevres
-print(moves_made[::-1])
-
-reward = 0
-
-s_moves=[]
-#solution
-for i  in range (10):
-    step = random.choice(moves)
-    s_moves.append(step)
-    if s_moves[i] != moves_made[i]:
-        reward = reward - 1
-
-    else:
-        reward = reward + 1
-
-print(reward)
