8a81cf6c144bdef2fb2f839d25097bcd43df497d
duckducksillygoose <Ashleigh.Gurr@student.curtin.edu.au>
2025-06-18 10:16:31 +0800

    Flattened out into one long array for ease of reinforcement learning.

--- Solver.py
+++ Solver.py
@@ -4,6 +4,14 @@ from model import *
 
 cube_import()
 
+def flatten_state():
+    color_map = {'O': 0, 'B': 1, 'R': 2, 'G': 3, 'Y': 4, 'W': 5, '*': 6}
+    flat = solve_state.flatten()
+    return np.array([color_map[c] for c in flat])
+
+def is_solved():
+    
+
 reward = 0 
 
 s_moves=[]
